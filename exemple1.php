c2c0cb439bea4ad95f737c9101844e49e2b6be4c
sirine <you@example.com>
2024-10-07 00:06:45 +0100

    first commit

---
+++ exemple1.php
@@ -0,0 +1,15 @@
+
+
+<?php
+$var ='bonjour';
+echo $var;
+echo 'je suis du code PHP';
+?>
+
+
+
+
+3-je n'arrive pas a trouver mon fichier 
+
+4-En ouvrant localhost/AtelierPHP/exemple1.php, le serveur web interprétera le PHP 
+et je voir le message . Cela montre que le serveur fonctionne correctement 
---
+++ exemple2.php
@@ -0,0 +1,8 @@
+<?php
+echo '<h1> titre de niveau 1</h1>';
+?>
+<h1>
+<?php
+echo ' titre de niveau 1';
+?>
+</h1>
